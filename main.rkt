c315bd5ecdc1fdb5ac852e31c53a350289daaa3b
Ben Greenman <benjaminlgreenman@gmail.com>
2017-10-21 23:39:35 -0400

    main: pretty good import script

--- main.rkt
+++ main.rkt
@@ -14,6 +14,8 @@
 
 (require
   file/glob
+  (only-in racket/file
+    copy-directory/files)
   racket/path
   (only-in racket/string
     string-join)
@@ -76,7 +78,7 @@
   (if (null? failed-to-run)
     (log-gtp-checkup-info "All benchmarks finished successfully")
     (begin
-      (log-gtp-checkup-info "Error running files:")
+      (log-gtp-checkup-info "Error running the following files:")
       (for ([fail (in-list failed-to-run)])
         (log-gtp-checkup-info "- ~a" fail)))))
 
@@ -86,14 +88,79 @@
 ;; -----------------------------------------------------------------------------
 
 (define (import-benchmark bm-dir)
+  (define not-gtp (why-not-gtp-dir bm-dir))
   (cond
-   [(gtp-dir? bm-dir)
-    (void)]
+   [not-gtp
+    (raise-user-error 'import-benchmark "failed to import directory '~a': ~a" bm-dir not-gtp)]
    [else
-    (raise-user-error 'import-benchmark "failed to import directory '~a', unrecognized directory structure")]))
+    (import/gtp-dir bm-dir)]))
+
+(define (import/gtp-dir dir)
+  (define program-name (directory-name-from-path dir))
+  (define typed-dir (build-path dir "typed"))
+  (define untyped-dir (build-path dir "untyped"))
+  (define base-dir (build-path dir "base"))
+  (define copy-both-dir
+    (let ([d (build-path dir "both")])
+      (if (directory-exists? d)
+        (λ (dest)
+          (for ([src (in-glob (build-path d "*"))])
+            (define name (file-name-from-path src))
+            (copy-directory/files src (build-path dest name))))
+        void)))
+  (define file* (racket-files typed-dir))
+  (define configuration (make-vector (length file*) 0))
+  (define new-dir (build-path PWD program-name))
+  (make-directory new-dir)
+  (when (directory-exists? base-dir)
+    (copy-directory/files base-dir (build-path new-dir "base")))
+  (let ([u-dir (build-path new-dir "untyped")])
+    (copy-directory/files untyped-dir u-dir)
+    (copy-both-dir u-dir))
+  (let ([t-dir (build-path new-dir "typed")])
+    (copy-directory/files typed-dir t-dir)
+    (copy-both-dir t-dir))
+  (for ([typed-file (in-list file*)]
+        [i (in-naturals)])
+    (define dirname (build-path new-dir (format "gradual-~a" i)))
+    (copy-directory/files untyped-dir dirname)
+    (copy-both-dir dirname)
+    (copy-file (build-path typed-dir typed-file) (build-path dirname typed-file) #true))
+  (log-gtp-checkup-info "finished importing '~a', please manually remove dependencies" program-name)
+  (void))
+
+(define (directory-name-from-path path)
+  (define-values [_base name _mbd] (split-path path))
+  (and (path? name) name))
 
 (define (gtp-dir? dir)
-  #false)
+  (not (why-not-gtp-dir dir)))
+
+;; Return a string explaining why `dir` is NOT a proper benchmark directory,
+;;  or `#false` if the directory is perfectly acceptable.
+(define (why-not-gtp-dir dir)
+  (define program-name (directory-name-from-path dir))
+  (define typed-dir (build-path dir "typed"))
+  (define untyped-dir (build-path dir "untyped"))
+  (cond
+   [(not program-name)
+    "could not parse directory name"]
+   [(directory-exists? (build-path PWD program-name))
+    "program already exists"]
+   [(not (directory-exists? typed-dir))
+    "missing 'typed/' directory"]
+   [(not (directory-exists? untyped-dir))
+    "missing 'untyped/' directory"]
+   [(not (equal? (racket-files typed-dir) (racket-files untyped-dir)))
+    "different '*.rkt' files in typed/ and untyped/ directories"]
+   [else
+    #false]))
+
+(define (racket-files dir)
+  (define filename*
+    (for/list ([full-path (in-glob (build-path dir "*.rkt"))])
+      (path-string->string (file-name-from-path full-path))))
+  (sort filename* string<=?))
 
 ;; =============================================================================
 
