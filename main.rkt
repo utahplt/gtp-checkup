6c6649456662c2bc26954b7f20f3fce78518e6f9
Ben Greenman <benjaminlgreenman@gmail.com>
2017-10-21 22:26:39 -0400

    main: checkpoint, runs correctly time to import

--- main.rkt
+++ main.rkt
@@ -1,14 +1,24 @@
 #lang racket/base
 
+;; TODO
+;; - time & space limits for 'raco' and 'racket'
+;; - quickstart for adding a benchmark
+
 (require racket/contract)
 (provide
   (contract-out
-    (gtp-checkup (-> racket-bin-dir/c void?))))
+    (gtp-checkup
+     (-> racket-bin-dir/c void?))
+    (import-benchmark
+     (-> directory-exists? void?))))
 
 (require
   file/glob
   racket/path
-  racket/system
+  (only-in racket/string
+    string-join)
+  (only-in racket/system
+    system*)
   racket/runtime-path)
 
 ;; =============================================================================
@@ -16,6 +26,8 @@
 (define-logger gtp-checkup)
 
 (define-runtime-path PWD ".")
+
+(define SEARCH-FOR-FILES-MATCHING "**/main.rkt")
 
 (define (contains-racket? d)
   (file-exists? (build-path d "racket")))
@@ -31,17 +43,23 @@
 ;; -----------------------------------------------------------------------------
 
 (define (gtp-checkup bin-dir)
-  (parameterize ([current-directory PWD])
-    (for ((main (in-glob "**/main.rkt")))
-      (checkup-file bin-dir main))))
+  (define results
+    (parameterize ([current-directory PWD])
+      (for/list ((main (in-glob SEARCH-FOR-FILES-MATCHING)))
+        (cons main (checkup-file bin-dir main)))))
+  (log-gtp-checkup-info "=== FINISHED ===")
+  (print-summary results))
 
 (define (checkup-file bin-dir main.rkt)
   (define-values [dir name _] (split-path main.rkt))
   (define rel-dir (find-relative-path (current-directory) dir))
   (log-gtp-checkup-info "Checking '~a'" rel-dir)
-  (and (raco-make bin-dir name)
-       (run-racket bin-dir name)
-       (void)))
+  (parameterize ([current-directory dir])
+    (and (log-gtp-checkup-info "compiling '~a'" name)
+         (raco-make bin-dir name)
+         (log-gtp-checkup-info "running '~a'" name)
+         (run-racket bin-dir name)
+         #true)))
 
 (define (raco-make bin name)
   (system* (build-path bin "raco") "make" name))
@@ -49,11 +67,48 @@
 (define (run-racket bin name)
   (system* (build-path bin "racket") name))
 
+(define (print-summary results)
+  (define cwd (current-directory))
+  (define failed-to-run
+    (for/list ([r (in-list results)]
+               #:when (not (cdr r)))
+      (path-string->string (find-relative-path cwd (car r)))))
+  (if (null? failed-to-run)
+    (log-gtp-checkup-info "All benchmarks finished successfully")
+    (begin
+      (log-gtp-checkup-info "Error running files:")
+      (for ([fail (in-list failed-to-run)])
+        (log-gtp-checkup-info "- ~a" fail)))))
+
+(define (path-string->string x)
+  (if (path? x) (path->string x) x))
+
+;; -----------------------------------------------------------------------------
+
+(define (import-benchmark bm-dir)
+  (cond
+   [(gtp-dir? bm-dir)
+    (void)]
+   [else
+    (raise-user-error 'import-benchmark "failed to import directory '~a', unrecognized directory structure")]))
+
+(define (gtp-dir? dir)
+  #false)
+
 ;; =============================================================================
 
 (module* main racket/base
   (require racket/cmdline (submod ".."))
+  (define cmd-mode (box 'checkup))
   (command-line
    #:program "gtp-checkup"
+   #:once-any
+   [("-n" "--new") "Import a new program" (set-box! cmd-mode 'import)]
    #:args (BIN-DIR)
-   (gtp-checkup BIN-DIR)))
+   (case (unbox cmd-mode)
+    [(import)
+     (import-benchmark BIN-DIR)]
+    [(checkup)
+     (gtp-checkup BIN-DIR)]
+    [else
+     (raise-user-error 'gtp-checkup "unknown mode '~a', goodbye" (unbox cmd-mode))])))
