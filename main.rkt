57087b27c3496fe017cae547bf4e85ac9ddaac93
Ben Greenman <benjaminlgreenman@gmail.com>
2019-04-09 11:19:05 -0400

    main: import only the worst-case typed config

--- main.rkt
+++ main.rkt
@@ -11,6 +11,7 @@
 (require
   file/glob
   (only-in racket/file
+    make-temporary-file
     delete-directory/files
     copy-directory/files)
   racket/path
@@ -18,6 +19,7 @@
     exn:fail:resource?
     with-deep-time-limit)
   (only-in racket/string
+    string-replace
     string-join)
   (only-in racket/system
     system*)
@@ -47,21 +49,16 @@
 (define BASE "base")
 (define BENCHMARKS "benchmarks")
 (define BOTH "both")
-(define GRADUAL~a "gradual-~a")
+(define TWC "typed-worst-case")
 (define TYPED "typed")
 (define UNTYPED "untyped")
 
 ;; -----------------------------------------------------------------------------
-(require (only-in racket/string string-contains?))
 
 (define (gtp-checkup bin-dir)
   (define results
     (parameterize ([current-directory PWD])
-      (for/list ((main (in-glob SEARCH-FOR-FILES-MATCHING))
-                 #:when (and
-                          (or (string-contains? (path-string->string main) "quadU")
-                              (string-contains? (path-string->string main) "quadT"))
-                          #;(not (string-contains? (path-string->string main) "gradual"))))
+      (for/list ((main (in-glob SEARCH-FOR-FILES-MATCHING)))
         (cons main (checkup-file bin-dir main)))))
   (log-gtp-checkup-info "=== FINISHED ===")
   (print-summary results))
@@ -116,6 +113,11 @@
 
 ;; -----------------------------------------------------------------------------
 
+;; import-benchmark : (-> directory-exists? void?)
+;; Copies files from the given directory to create a new checkup benchmark.
+;; The checkup version has ONLY the typed configuration, and uses
+;;  require/typed instead of require/typed/check to put a boundary between
+;;  every pair of modules.
 (define (import-benchmark bm-dir)
   (define not-gtp (why-not-gtp-dir bm-dir))
   (cond
@@ -124,32 +126,23 @@
    [else
     (import/gtp-dir bm-dir)]))
 
-(define (import/gtp-dir dir)
-  (define program-name (directory-name-from-path dir))
-  (define typed-dir (build-path dir TYPED))
-  (define untyped-dir (build-path dir UNTYPED))
+(define (import/gtp-dir src-dir)
+  (define program-name (directory-name-from-path src-dir))
+  (define typed-dir (build-path src-dir TYPED))
   (define copy-base-dir
-    (make-copy-dir (build-path dir BASE)))
+    (make-copy-dir (build-path src-dir BASE)))
   (define copy-both-dir
-    (make-copy-dir (build-path dir BOTH)))
+    (make-copy-dir (build-path src-dir BOTH)))
   (define file* (racket-files typed-dir))
-  (define configuration (make-vector (length file*) 0))
   (define new-dir (build-path PWD BENCHMARKS program-name))
-  (make-directory new-dir)
+  (void
+    (make-directory new-dir))
   (copy-base-dir (build-path new-dir BASE))
-  (let ([u-dir (build-path new-dir UNTYPED)])
-    (copy-directory/files untyped-dir u-dir)
-    (copy-both-dir u-dir))
-  (let ([t-dir (build-path new-dir TYPED)])
-    (copy-directory/files typed-dir t-dir)
-    (copy-both-dir t-dir))
-  (for ([typed-file (in-list file*)]
-        [i (in-naturals)])
-    (define dirname (build-path new-dir (format GRADUAL~a i)))
-    (copy-directory/files untyped-dir dirname)
-    (copy-both-dir dirname)
-    (copy-file (build-path typed-dir typed-file) (build-path dirname typed-file) #true))
-  (log-gtp-checkup-info "finished importing '~a', please manually remove dependencies" program-name)
+  (let ([new-t-dir (build-path new-dir TWC)])
+    (copy-directory/files typed-dir new-t-dir)
+    (copy-both-dir new-t-dir)
+    (remove-require-typed-check new-t-dir))
+  (log-gtp-checkup-info "finished importing '~a', please check that everything looks good and that 'main.rkt' runs" program-name)
   (void))
 
 (define (make-copy-dir d)
@@ -194,6 +187,26 @@
     (for/list ([full-path (in-glob (build-path dir "*.rkt"))])
       (path-string->string (file-name-from-path full-path))))
   (sort filename* string<=?))
+
+(define (remove-require-typed-check dir)
+  (for ((fn (in-glob (build-path dir "*.rkt"))))
+    (remove-require-typed-check/file fn)))
+
+(define (remove-require-typed-check/file fn)
+  (define tmp (make-temporary-file))
+  (with-output-to-file tmp #:exists 'append
+    (lambda ()
+      (with-input-from-file fn
+        (lambda ()
+          (for ((ln (in-lines)))
+            (writeln (string-replace* ln '(("require-typed-check" " ") ("require/typed/check" "require/typed")))))))))
+  (copy-file tmp fn)
+  (void))
+
+(define (string-replace* str ft* #:all? [all? #true])
+  (for/fold ((acc str))
+            ((ft (in-list ft*)))
+    (string-replace acc (car ft) (cadr ft) #:all? all?)))
 
 ;; =============================================================================
 
